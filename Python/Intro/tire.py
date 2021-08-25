fd4fa54f939ff76838bae43e336b72061eaa8746
Will Graham <wgraham800@gmail.com>
2021-08-25 07:30:05 -0600

    Added SnowTire child class

--- Python/Intro/tire.py
+++ Python/Intro/tire.py
@@ -4,14 +4,12 @@ class Tire:
     Tire represents a tire that could be used on a car.
     """
 
-    def __init__(self, width, ratio, diameter, brand='Michelin', speed_rating='H', standard='P', tire_type='all-weather'):
+    def __init__(self, width, ratio, diameter, brand='Michelin', standard='P'):
         self.standard = standard
         self.width = width
         self.ratio = ratio
         self.diameter = diameter
         self.brand = brand
-        self.speed_rating = speed_rating
-        self.tire_type = tire_type
 
     def circumference(self):
         """
@@ -22,17 +20,39 @@ class Tire:
         80.1
         """
 
-        side_wall_inches = (self.width * (self.ratio / 100)) / 25.4
-        total_diameter = side_wall_inches * 2 + self.diameter
+        total_diameter = self._side_wall_inches() * 2 + self.diameter
         return round(total_diameter * math.pi, 1)
 
 
     def __repr__(self):
         """
         Represent the tire's information in the standard notion
-        on the side of the tire. Example: 'Michelin snow tire: P215/65R15'
+        on the side of the tire. Example: 'Michelin tire: P215/65R15'
         """
 
-        return (f"{self.brand} {self.tire_type} tire: "
-            + f"{self.standard}{self.width}/{self.ratio}"
-            + f"R{self.diameter} {self.speed_rating}")
+        return (f"{self.brand} tire: "
+            + f"{self.standard}{self.width}/{self.ratio} R{self.diameter}")
+
+    def _side_wall_inches(self):
+        return (self.width * (self.ratio / 100)) / 25.4
+
+
+class SnowTire(Tire):
+    def __init__(self, width, ratio, diameter, chain_thickness, brand='Michelin', standard='P'):
+        super().__init__(width, ratio, diameter, brand, standard)
+        self.chain_thickness = chain_thickness
+
+    def circumference(self):
+        """
+        The circumference of a tire with snow chains, in inches.
+
+        >>> tire = SnowTire(205, 65, 15, 2)
+        >>> tire.circumference()
+        92.7
+        """
+
+        total_diameter = (self._side_wall_inches() + self.chain_thickness) * 2 + self.diameter
+        return round(total_diameter * math.pi, 1)
+
+    def __repr__(self):
+        return super().__repr__() + " (Snow)"
